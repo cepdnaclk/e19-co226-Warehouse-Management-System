6a16d3d0a7c15f189857f0fdb8ef1450b2482527
udayaKavinda <e19155@eng.pdn.ac.lk>
2023-08-03 12:38:17 +0530

    gitignore added

--- Backend/src/main/java/com/databaseWHM/Warehouse/Management/System/model/Customer.java
+++ Backend/src/main/java/com/databaseWHM/Warehouse/Management/System/model/Customer.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data//getters and setters
+@NoArgsConstructor//no argument constructor
+@AllArgsConstructor//constructor
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,60 +22,4 @@ public class Customer {
     private String AddressLine3;
     private String PhoneNumber;
     private String Email;
-
-    public int getCustomerID() {
-        return CustomerID;
-    }
-
-    public void setCustomerID(int customerID) {
-        CustomerID = customerID;
-    }
-
-    public String getCustomerName() {
-        return CustomerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        CustomerName = customerName;
-    }
-
-    public String getAddressLine1() {
-        return AddressLine1;
-    }
-
-    public void setAddressLine1(String addressLine1) {
-        AddressLine1 = addressLine1;
-    }
-
-    public String getAddressLine2() {
-        return AddressLine2;
-    }
-
-    public void setAddressLine2(String addressLine2) {
-        AddressLine2 = addressLine2;
-    }
-
-    public String getAddressLine3() {
-        return AddressLine3;
-    }
-
-    public void setAddressLine3(String addressLine3) {
-        AddressLine3 = addressLine3;
-    }
-
-    public String getPhoneNumber() {
-        return PhoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        PhoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
 }

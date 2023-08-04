b7f255f322711deeacc10f21a76f8df63e534a69
hashinivish <e19142@eng.pdn.ac.lk>
2023-08-04 22:51:10 +0530

    frontendnew

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard.jsx';
+import Customers from './pages/Customers';
+import Product from './pages/Product.jsx';
+import Orders from './pages/Orders.jsx';
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     <div className="AppGlass">
-      <Sidebar/>
-     </div>
-    </div>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
